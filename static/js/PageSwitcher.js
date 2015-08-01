47ad67e77aa24ed9e26cd610f3c5421aa1bc18b6
Jan Schütze <jan.schuetze@exozet.com>
2015-08-01 13:19:57 +0200

    PageSwitcher in eigene Komponente ausgelagert

---
+++ static/js/PageSwitcher.js
@@ -0,0 +1,52 @@
+define('PageSwitcher', ['jquery', "json!../../config.json"], function($, config) {
+
+    var PageSwitcher = function(domElement, options) {
+        var that = this;
+        this.domElement = $(domElement);
+
+        var current_page = 0;
+        var waiting_time = 3;
+        var max_progress = waiting_time;
+        var new_page_in = waiting_time;
+
+        this.pages = this.domElement.find('.js_page');
+        this.pages.hide();
+        $(this.pages[0]).show();
+
+        var toNextPage = function() {
+            new_page_in--;
+            if (new_page_in == 0)
+            {
+                $(that.pages[current_page]).hide();
+
+                /* Remember this, in case every page would be hidden. */
+                var initial_current_page = current_page;
+
+                do {
+                    current_page = current_page + 1;
+                    console.log('FINDING NEXT PAGE for', initial_current_page, ' and testing ', current_page);
+                    if (that.pages.length == current_page)
+                    {
+                        current_page = 0;
+                    }
+
+                } while ($(that.pages[current_page]).hasClass('is-hidden') && initial_current_page !== current_page);
+
+                console.log('NEXT PAGE after', initial_current_page, 'is now', current_page);
+
+                $(that.pages[current_page]).show();
+
+                new_page_in = max_progress;
+                that.domElement.find('.js_progress_bar').css('width', 0);
+            }
+            else
+            {
+                that.domElement.find('.js_progress_bar').css('width', Math.floor(100 - 100 * (new_page_in / max_progress)) + '%');
+            }
+        };
+
+        setInterval(toNextPage, (waiting_time / max_progress) * 1000);
+    };
+
+    return PageSwitcher;
+});
--- static/js/ShowRooms.js
+++ static/js/ShowRooms.js
@@ -146,49 +146,7 @@ define('ShowRooms', ["jquery", "json!../../config.json", "moment"], function ($,
         $('.js_campus').val(that.campus);
         $('.js_house').val(that.house);
 
-        var current_page = 0;
-        var waiting_time = 10;
-        var max_progress = waiting_time;
-        var new_page_in = waiting_time;
-
-        this.pages = this.dom_element.find('.js_page');
-        this.pages.hide();
-        $(this.pages[0]).show();
-
-        var toNextPage = function() {
-            new_page_in--;
-            if (new_page_in == 0)
-            {
-                $(that.pages[current_page]).hide();
-
-                /* Remember this, in case every page would be hidden. */
-                var initial_current_page = current_page;
-
-                do {
-                    current_page = current_page + 1;
-
-                    if (that.pages.length == current_page)
-                    {
-                        current_page = 0;
-                    }
-
-                } while ($(that.pages[current_page]).hasClass('is-hidden') || initial_current_page === current_page);
-
-                $(that.pages[current_page]).show();
-
-                new_page_in = max_progress;
-                $('.progress-bar').css('width', 0);
-            }
-            else
-            {
-                $('.progress-bar').css('width', Math.floor(100 - 100 * (new_page_in / max_progress)) + '%');
-            }
-        };
-
-        setInterval(toNextPage, (waiting_time / max_progress) * 1000);
-
         setInterval(function() {
-            that.logDebug('refresh!');
             that.refreshNowValue();
             that.refreshReservations();
         }, 60 * 1000);
