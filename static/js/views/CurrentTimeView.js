145846d5901b065d15543026e8e9328058aa7155
Alexander Kiy <alekiy@uni-potsdam.de>
2017-02-16 13:46:31 +0100

    added clock js for time disaply

--- static/js/views/CurrentTimeView.js
+++ static/js/views/CurrentTimeView.js
@@ -1,23 +1,22 @@
-define('views/CurrentTimeView', ['jquery', "config", "moment", "views/BaseView"], function($, config, moment, BaseView) {
+define('views/CurrentTimeView', ['jquery', "config", "moment", "bandClock", "views/BaseView"], function($, config, moment, bandClock, BaseView) {
 
     var CurrentTimeView = BaseView.extend({
         initialize: function() {
             var that = this;
             that.render();
-            
-            // updating current time every secound
-            setInterval(function() {
-                config.refreshTime();
-                that.render();
-            }, 1000);
         },
 
         render: function() {
-            var currentTimeString = moment().format('HH:mm');
-
-            if ($(this.el).text() != currentTimeString) {
-                $(this.el).text(currentTimeString);
-            }
+            $('.bandClock').bandClock({
+              //configure here
+              size: 125,
+              strokeColor: '#fff', 
+              bgColor: '#d0dae3',
+              color: '#014260' ,
+              lineCap: 2,
+              gap: 0,
+              fontStyle: '32px Helvetica Neue, Helvetica, Arial, sans-serif'
+            });
         }
     });
 
