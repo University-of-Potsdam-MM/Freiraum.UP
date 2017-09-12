4d4044809a656def4ac279555e2dbb7ff661c53a
Alexander Kiy <alekiy@uni-potsdam.de>
2017-09-12 11:10:30 +0200

    fixed typo

--- test/api-tests.js
+++ test/api-tests.js
@@ -48,7 +48,7 @@ before(function(done) {
             'moment'
         ],
         function(config, moment) {
-            config.url =config.get('build-dir')+'config.json';
+            config.url =config.get('builddir')+'/config.json';
             config.fetch({async:false});
             /*
              * wir testen immer am nächsten Montag um 10:00 Uhr
