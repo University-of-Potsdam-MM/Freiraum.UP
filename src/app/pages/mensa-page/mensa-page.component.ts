1eaaafe2437fccb7571189405c85e6339caff472
Ludwig Weihmann <ludwig.kraeh@uni-potsdam.de>
2020-05-20 19:05:52 +0200

    added iteration for multiple canteens in non interactive mode

--- src/app/pages/mensa-page/mensa-page.component.ts
+++ src/app/pages/mensa-page/mensa-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Meal, MensaResponse} from '../../../types/mensa.response';
 import {BasicPageComponent} from '../../components/basic-page/basic-page.component';
+import {iterableArray} from '../../util/iterableArray';
 
 @Component({
   selector: 'app-mensa-page',
@@ -11,16 +12,29 @@ export class MensaPageComponent extends BasicPageComponent  implements OnInit {
 
   selectedCanteenIndex = 0;
   meals: {[canteen: string]: Meal[]} = {};
+  canteensIterator;
+  enabledCanteens = [];
 
   constructor() { super('mensa'); }
 
   ngOnInit() {
-    for (const canteen of this.config.mensa.canteens) {
+    this.enabledCanteens = this.config.mensa.canteens.filter(m => m.enabled);
+    for (const canteen of this.enabledCanteens) {
        this.api.feeds.mensa[canteen.name].subscribe(
          (response: MensaResponse) => {
            this.meals[canteen.name] = response.meal;
          }
        );
+    }
+
+    if (this.config.mensa.canteens.length > 1) {
+      this.canteensIterator = iterableArray(this.enabledCanteens);
+    }
+  }
+
+  onSelected() {
+    if (this.canteensIterator) {
+      this.selectedCanteenIndex = this.enabledCanteens.indexOf(this.canteensIterator.next().value);
     }
   }
 }
